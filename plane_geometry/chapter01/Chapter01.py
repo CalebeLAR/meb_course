63edb267198bed3273a1d782dfd12bf066af10b5
calebeLAR <ca6ele@gmail.com>
2024-02-07 10:40:03 -0300

    renemed files

--- plane_geometry/chapter01/Chapter01.py
+++
@@ -1,9 +0,0 @@
-class Chapter01:
-    exercises = []
-    topics = [
-        "CAPÍTULO I — Introdução",
-        "I. Conceitos primitivos e postulados",
-        "II. Determinação de plano",
-        "III. Posições das retas",
-        "IV. Interseção de planos",
-    ]
