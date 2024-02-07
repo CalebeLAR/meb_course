a5da1f1802730a488a346fc1e5d1c8275ed41b6c
calebeLAR <ca6ele@gmail.com>
2024-02-07 10:40:29 -0300

    chapter01

---
+++ spatial_geometry/chapter01/Chapter01.py
@@ -0,0 +1,9 @@
+class Chapter01:
+    exercises = []
+    topics = [
+        "CAPÍTULO I — Introdução",
+        "I. Conceitos primitivos e postulados",
+        "II. Determinação de plano",
+        "III. Posições das retas",
+        "IV. Interseção de planos",
+    ]
