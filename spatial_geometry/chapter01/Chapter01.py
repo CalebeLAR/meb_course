e80b86f0462d20fb3baeadedcdff1a14e9802c8f
calebeLAR <ca6ele@gmail.com>
2024-02-09 11:35:12 -0300

    chapter01

--- spatial_geometry/chapter01/Chapter01.py
+++ spatial_geometry/chapter01/Chapter01.py
@@ -1,9 +1,15 @@
 class Chapter01:
-    exercises = []
+    exercises = [
+        'i3_posicoes_das_retas.txt',
+        'i2_intersecao_de_plano.txt',
+    ]
     topics = [
         "CAPÍTULO I — Introdução",
         "I. Conceitos primitivos e postulados",
         "II. Determinação de plano",
         "III. Posições das retas",
+        "12. Retas reversas",
+        "13. Quadrilátero reverso",
+        "15. Posições relativas de duas retas",
         "IV. Interseção de planos",
     ]
